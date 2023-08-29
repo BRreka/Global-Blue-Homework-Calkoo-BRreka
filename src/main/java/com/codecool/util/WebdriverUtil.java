4347d4cb50c8129d6bc6d8e8a5a3c692250e1940
BRreka <benczereka9@gmail.com>
2023-08-29 12:24:06 +0200

    write webdriver utility class as singleton pattern

--- src/main/java/com/codecool/util/WebdriverUtil.java
+++ src/main/java/com/codecool/util/WebdriverUtil.java
@@ -1,4 +1,52 @@
 package com.codecool.util;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+
+import java.nio.file.*;
+import java.util.*;
+
 public class WebdriverUtil {
+
+    private static volatile WebDriver webDriverInstance = null;
+    private WebdriverUtil() {}
+
+    public static WebDriver getInstance() {
+        WebDriver driver = webDriverInstance;
+        if(driver == null) {
+            synchronized (WebdriverUtil.class) {
+                driver = webDriverInstance;
+                if(driver == null) {
+                    if(isFirefoxInstalled()) {
+                        WebDriverManager.firefoxdriver().setup();
+                        webDriverInstance = driver = new FirefoxDriver();
+                    } else if (isChromeInstalled()) {
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        options.addArguments("--remote-allow-origins=*");
+                        webDriverInstance = driver = new ChromeDriver(options);
+                    } else {
+                        throw new RuntimeException("Sorry, your browser isn't supported.");
+                    }
+                }
+            }
+        }
+        return driver;
+    }
+
+    private static boolean isChromeInstalled() {
+        Optional<Path> browserPath = WebDriverManager.chromedriver().getBrowserPath();
+        return browserPath.isPresent();
+    }
+
+    private static boolean isFirefoxInstalled() {
+        Optional<Path> browserPath = WebDriverManager.firefoxdriver().getBrowserPath();
+        return browserPath.isPresent();
+    }
+    public static void quit() {
+        webDriverInstance.quit();
+        webDriverInstance = null;
+    }
 }
