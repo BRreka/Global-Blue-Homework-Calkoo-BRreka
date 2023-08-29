2110f2810e06c17680c2621ec157ccf83571f002
BRreka <benczereka9@gmail.com>
2023-08-29 12:32:05 +0200

    add webdriver to vat calculator page object

--- src/main/java/com/codecool/pages/VatCalculatorPage.java
+++ src/main/java/com/codecool/pages/VatCalculatorPage.java
@@ -1,3 +1,10 @@
 package com.codecool.pages;
+
+import com.codecool.util.WebdriverUtil;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
 public class VatCalculatorPage {
+    WebDriver driver = WebdriverUtil.getInstance();
+
 }
