cc3fa15a318fa0a5158f01f9548aae443820d543
BRreka <benczereka9@gmail.com>
2023-08-29 13:07:06 +0200

    add method to load calculator page

--- src/main/java/com/codecool/pages/VatCalculatorPage.java
+++ src/main/java/com/codecool/pages/VatCalculatorPage.java
@@ -4,9 +4,16 @@ import com.codecool.util.WebdriverUtil;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 
+import java.time.Duration;
+
 public class VatCalculatorPage {
     WebDriver driver = WebdriverUtil.getInstance();
     public VatCalculatorPage() {
         PageFactory.initElements(driver, this);
+    }
+    public void loadCalculatorPage(String url) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
     }
 }
